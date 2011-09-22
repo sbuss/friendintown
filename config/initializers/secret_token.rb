88bf7e982d4069e9bb0fb08e2c7ed3e8b12d8511
Devin Blais <devinblais+git@gmail.com>
2011-09-22 05:45:08 -0700

    rails base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendInTown::Application.config.secret_token = 'd5eb9fc817756de57b3105ff75f2e9269a3ea722cb97d0ac4e03fe658447ab7ad6f680d23958d82b6197671d0a44f16abadf7e5eedc351021d6fe8dc90b5fb83'
