c5cdf3b40ea4e26d5e8e6f919ee4bae14901fdf9
Steven <steven.buss@gmail.com>
2011-09-28 22:38:37 -0700

    Initial commit of signup page

---
+++ signup_placeholder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SignupPlaceholder::Application.config.secret_token = 'c25dc788ee534622a225443892453570e4a39ca1c79389b77d3ee2a5a3458c610f1bc8709ecf16944be790b6a34b60328cdf4bcaec956b7ca7ae7f3b4d74183c'
