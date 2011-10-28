c6b5f460c3e803e89359a2d63988a253fe7d31af
Steven <steven.buss@gmail.com>
2011-10-28 00:19:30 -0700

    Made either rating or comment mandatory for feedback.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,11 +1,12 @@
 class Feedback < ActiveRecord::Base
   belongs_to :user
   
-  validates :comment, :presence => true,
-                      :length   => { :minimum => 5 }
-  validates :score, :presence     => true,
-                    :numericality => { :greater_than => 0,
-                                       :less_than => 6 }
+  validates :comment, :length   => { :minimum => 5 }, 
+                      :unless => Proc.new {|obj| obj.comment.blank? }
+  validates :score, :numericality => { :greater_than => 0,
+                                       :less_than => 6 }, 
+                    :unless => proc {|obj| obj.score.blank? }
+  validate :score_or_comment
 
   VALID_RATINGS = (1..5).to_a
 
@@ -18,4 +19,11 @@ class Feedback < ActiveRecord::Base
       :comment => self.comment
     }
   end
+
+  private
+    def score_or_comment
+      if score.blank? && comment.blank?
+        errors.add_to_base("Either a star rating or a comment is required.")
+      end
+    end
 end
